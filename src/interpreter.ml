bcf4d137470fac7fe6318f8d2ad821730b44bf07
daviidsilvaa <davidsfernandes2@gmail.com>
2019-04-01 14:12:51 -0300

    aula 27.03

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,3 +1,21 @@
 
+open Absyn
+
+type memory = (id, float) Hashtbl.t
+
+let rec run mem prog =
+	match prog with
+	| CompoundStm (stm1, stm2) -> run mem stm1; run mem stm2
+	| AssignStm (var, e) -> Hashtbl.replace mem var (eval e)
+	| PrintStm list -> List.iter (fun e -> print_float (eval mem e); print_char ' ') list
+	and eval mem exp =
+		match exp with
+		| IdExp var -> Hashtbl.find mem var
+      | NumExp cte -> cte
+      (*| OpExp (cte1, op, cte2) -> of exp * binop * exp*)
+      (*| EseqExp of stm * exp*)
+
 let interpret prog =
-  ()
+	let mem = Hashtbl.create 0 in
+	run mem prog	
+	
--- src/maxargs.ml
+++ src/maxargs.ml
@@ -1,3 +1,14 @@
 open Absyn
 
-let maxargs statement = 0
+let rec maxargs statement =
+   match statement with
+   | CompoundStm (stm1, stm2) -> max (maxargs stm1) (maxargs stm2)
+   | AssignStm (_, e) -> maxargs_exp e
+   | PrintStm args -> List.fold_left max (List.length args) (List.map maxargs_exp args)
+
+and maxargs_exp expression =
+	match expression with
+	| IdExp id -> 0
+	| NumExp num -> 0
+	| OpExp (e1, _, e2) -> max (maxargs_exp e1) (maxargs_exp e2)
+	| EseqExp (s, e) -> max (maxargs s) (maxargs_exp e)
